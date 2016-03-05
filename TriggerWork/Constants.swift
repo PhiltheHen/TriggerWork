6961a96556c5cb29da38e56ab497a486547ed946
Phil Henson <phil@cellscope.com>
2016-03-05 13:25:10 -0800

    TA6-16. Setup build schemes: Staging, Production and App Store.

---
+++ TriggerWork/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  TriggerWork
+//
+//  Created by Phil Henson on 3/5/16.
+//  Copyright © 2016 Craftsbury Outdoor Center. All rights reserved.
+//
+
+import Foundation
+
+#if STAGING
+    let firebaseURL = "triggerworkstaging.firebaseIO.com"
+#else
+    let firebaseURL = "triggerwork.firebaseIO.com"
+#endif
---
+++ TriggerWork/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  TriggerWork
+//
+//  Created by Phil Henson on 3/5/16.
+//  Copyright © 2016 Craftsbury Outdoor Center. All rights reserved.
+//
+
+import Foundation
