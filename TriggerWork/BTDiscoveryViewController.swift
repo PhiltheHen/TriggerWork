6059ead4085ca3f68e922479b5f8dbb1299e0f54
Phil Henson <phil@cellscope.com>
2016-03-05 16:24:33 -0800

    TA6-7. Added tab bar controller and modal screen for connecting to bluetooth devices.

---
+++ TriggerWork/BTDiscoveryViewController.swift
@@ -0,0 +1,50 @@
+//
+//  BTDiscoveryViewController.swift
+//  TriggerWork
+//
+//  Created by Phil Henson on 3/5/16.
+//  Copyright © 2016 Craftsbury Outdoor Center. All rights reserved.
+//
+
+import UIKit
+
+class BTDiscoveryViewController: UIViewController {
+
+    @IBOutlet weak var tableContainerView: UIView!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var emptyStateView: UIView!
+    
+    var areDevicesAvailable = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // TODO: create BTUtility class
+        
+        //BTUtility.checkForDevices();
+        
+        tableContainerView.hidden = !areDevicesAvailable
+
+    }
+
+    @IBAction func scanDevicesDidTouch(sender: UIButton) {
+        
+    }
+    
+    // MARK: UI Settings
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
