6cb2a7f1fdbac89645336c65dceff1d3cbd6ec96
Phil Henson <phil@cellscope.com>
2016-06-02 03:32:19 -0700

    No ticket. Added untracked files.

---
+++ Pods/Target Support Files/Pods-TriggerWork/Pods-TriggerWork-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TriggerWork : NSObject
+@end
+@implementation PodsDummy_Pods_TriggerWork
+@end
---
+++ Pods/Target Support Files/Pods-TriggerWorkTests/Pods-TriggerWorkTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TriggerWorkTests : NSObject
+@end
+@implementation PodsDummy_Pods_TriggerWorkTests
+@end
